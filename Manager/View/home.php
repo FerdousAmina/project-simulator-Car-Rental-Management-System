e27a5bfe67ee194c7505909d559c83228a23a6d6
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-03 00:39:41 +0600

    home nav bar added

--- Manager/View/home.php
+++ Manager/View/home.php
@@ -4,4 +4,11 @@
         <meta charset="UTF-8">
         <title>CAR RENTAL MANAGEMENT</title>
     </head>
+<body>
+    <nav>
+        <div>
+            <a href="home.php">Home</a>
+        </div>
+    </nav>
+
 </html>
