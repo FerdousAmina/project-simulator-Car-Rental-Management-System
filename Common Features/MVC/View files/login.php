f020b44e079f635f857d6ba8a0e09d0222ebf747
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-08 23:28:38 +0600

    login page

--- Common Features/MVC/View files/login.php
+++ Common Features/MVC/View files/login.php
@@ -5,7 +5,10 @@
 </head>
 <body>
     Username:<input type="text" name="username"><br><br>
-    Password:<input type="text" name="password">
+    Password:<input type="text" name="password"><br><br>
+    <input type="checkbox" name="remember">Remember me <br><br>
+    <input type ="submit" value="Login">
+
 </body>
 
 
