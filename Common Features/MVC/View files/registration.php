73c87c3ced2a5523289fc81cb7c3f9ff669adb69
Amina <aminaferdous1120@gmail.com>
2026-01-09 01:28:57 +0600

    commit for registration

--- Common Features/MVC/View files/registration.php
+++ Common Features/MVC/View files/registration.php
@@ -6,6 +6,12 @@
 </head>
 
 <body>
+    <h2>Register</h2>
+    Full Name: <input type="text"><br><br>
+    User Name: <input type="text"><br><br>
+    Email Address: <input type="text"><br><br>
+    Password: <input type="text"><br><br>
+    Confirm Password: <input type="text"><br><br>
 
 </body>
 </html>
