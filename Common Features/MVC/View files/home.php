32a891e674fb3a80e35eb723b1c0eef47319e4a4
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-08 23:02:41 +0600

    commit

---
+++ Common Features/MVC/View files/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>CAR RENTAL MANAGEMENT</title>
+        <link rel="stylesheet" href="../CSS/loginbar.css">
+        <link rel="stylesheet" href="../CSS/body.css">
+        <link rel="stylesheet" href="../CSS/nav.css">
+        <link rel="stylesheet" href="../CSS/home.css">
+    </head>
+<body>
+    <nav>
+        <div>
+            <a href="home.php"class="nav-home">Home</a>
+            &nbsp;<a href="about.php"Class="nav-about" onclick="alert('This is a car rental management system develop by tahabi');return false;">About</a>
+            <a href="login.php" class="nav-login">Login</a>
+        </div>
+    </nav>
+
+    <div class="home">
+        <h1>Rent Your Dream Car Easily</h1><br>
+        <p>Simple . Fast . Affordable</p>
+    </div>
+
+</html>
---
+++ Common Features/MVC/View files/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+</head>
+<body>
+    Username:<input type="text" name="username"> 
+</body>
+
+
+<html>
+
