975c87512b7d515cfc563bc441bc93742a9656c6
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-09 00:23:07 +0600

    commit

--- Common Features/MVC/View files/login.php
+++ Common Features/MVC/View files/login.php
@@ -2,15 +2,18 @@
 <html>
     <head>
         <title>Login</title>
+        <link rel="stylesheet" href="../Stylesheets/loginpage.css">
 </head>
 <body>
+    <form>
+    <h2>Login</h2>
     Username:<input type="text" name="username"><br><br>
     Password:<input type="text" name="password"><br><br>
     <input type="checkbox" name="remember">Remember me <br><br>
     <input type ="submit" value="Login">
 
     <p>Not registred? <a href="registration.php">Register Here</a></p>
-
+    </form>
 </body>
 
 
