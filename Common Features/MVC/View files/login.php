13b002ace181ee33aabd8e638c0d755af81e9cda
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-08 23:38:28 +0600

    Updated registration link

--- Common Features/MVC/View files/login.php
+++ Common Features/MVC/View files/login.php
@@ -9,6 +9,8 @@
     <input type="checkbox" name="remember">Remember me <br><br>
     <input type ="submit" value="Login">
 
+    <p>Not registred? <a href="registration.php">Register Here</a></p>
+
 </body>
 
 
