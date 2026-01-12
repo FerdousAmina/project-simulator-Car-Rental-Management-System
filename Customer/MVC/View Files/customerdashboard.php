2d349af6bb947391ceb5feacdfcb2b4d2eed0b42
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-12 22:35:05 +0600

    dashboard

--- Customer/MVC/View Files/customerdashboard.php
+++ Customer/MVC/View Files/customerdashboard.php
@@ -8,7 +8,10 @@
 <body>
     <div class="sidebar">
         <h2>Features</h2>
-        
-
-
+        <a href="places.php">Nearby Places to Visit</a>
+        <a href="calculator.php">Cost Calculator</a>    
+        <a href="tripnotes.php">My Notes</a>
+        <a href="feedback.php">Feedback</a>
+        <a href="emergencydirectory.php">Emergency Helplines</a>
+    </div>
 </body>    
