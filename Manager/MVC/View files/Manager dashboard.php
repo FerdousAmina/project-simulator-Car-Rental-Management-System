5a00a9b5e7d85bbabd7c2fde4079d627e832e667
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-13 01:31:19 +0600

    commit

---
+++ Manager/MVC/View files/Manager dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Manager Dashboard</title>
+    <link rel="stylesheet" href="../stylesheets/Manager dashboard.css">
+</head>
+<body>
+
+<div class="sidebar">
+    <h1>Features</h1>
+    <a href="Notice Board Management.php">Notice</a>
+    <a href="Service center Diractory.php">Service Center</a>
+    <a href="Drivers Information.php">Drivers</a>
+    <a href="About us.php">About</a>
+    <a href="Staff attandence.php">Staff Attandence</a>
+    <a href="Logout.php">Logout</a>
+    
+</div>
+</body>
+</html>
