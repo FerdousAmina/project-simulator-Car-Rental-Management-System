1861fe7c226afbf06ba3658878af805b5cbdcc6b
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-12 22:23:39 +0600

    commit dashboard

--- Customer/MVC/View Files/customerdashboard.php
+++ Customer/MVC/View Files/customerdashboard.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+     <meta charset="UTF-8">
     <title>Customer Dashboard</title>
-</head>    
+</head>
+
+<body>
+    <div class="sidebar">
+        <h2>Features</h2>
+        
+
+
+</body>    
