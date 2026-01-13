bb2db87123893d9fc1e16e8cdd329e0f148a33ae
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-14 02:29:35 +0600

    commit

---
+++ Manager/MVC/View files/add_controller.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add driver</title>
+</head>
+<body>
+    <form id="driverForm" method="post" action="../Controller logic/add_controller.php" onsubmit="return validateForm()">
+    <h1> Add New Driver </h2>
+    Name:<input type="text" id="name" name="name"><br><br>
+    Phone:<input type="text" id="phone" name="phone"><br><br>
+    nid:<input type="text" id="nid" name="nid"><br><br>
+    Age:<input type="number" id="age" name="age"><br><br>
+
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
