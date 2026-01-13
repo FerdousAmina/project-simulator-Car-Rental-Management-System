927ab2734987f5c2e60173226f8681627642b861
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-13 19:01:09 +0600

    commit

---
+++ Customer/MVC/View Files/emergencydirectory.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+    <title>Emergency Directory</title>
+    <link rel="stylesheet" href="../Stylesheets/emergencydirectory.css">
+</head>
