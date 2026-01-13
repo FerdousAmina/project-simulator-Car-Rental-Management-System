6f286c98c4403c7245db5ca0b0ca075b8171734f
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-14 02:40:12 +0600

    commit

---
+++ Common Features/MVC/JavaScript Files/loginvalidation.js
@@ -0,0 +1,19 @@
+function handleSubmit(){
+    var username = document.getElementById("username").value.trim();
+    var password = document.getElementById("password").value.trim();
+    
+    var errorDiv = document.getElementById("error");
+    var outputDiv = document.getElementById("output");
+
+    errorDiv.innerHTML = "";
+    outputDiv.innerHTML = "";
+
+    if(username === "" || password === ""){
+        errorDiv.innerHTML = "Please fill in all fields.";
+        return false;
+    } 
+
+    outputDiv.innerHTML = "Login Successful!";
+
+    return false;
+}  
