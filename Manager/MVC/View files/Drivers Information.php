ba45c9dce041f717df0c82850761402eba636fc5
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-14 01:15:57 +0600

    drivers table

---
+++ Manager/MVC/View files/Drivers Information.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Driver Management</title>
+  </head>
+<body>
+    <h1>Driver List</h1>
+    <a href="add_controller.php" class="adddriver"> Add New Driver</a><br><br>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>NID</th>
+            <th>Age</th>
+            <th>Action<th>
+        </tr>
+    </table>
+</body>
+</html>
