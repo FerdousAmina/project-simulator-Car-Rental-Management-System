4f5c435a6f279b1afa26f39bf4420b4dc5474e50
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-14 02:29:53 +0600

    commit

--- Common Features/MVC/View files/login.php
+++ Common Features/MVC/View files/login.php
@@ -5,17 +5,19 @@
         <link rel="stylesheet" href="../Stylesheets/loginpage.css">
 </head>
 <body>
-    <form>
+    <form onsubmit="return handleSubmit()">
     <h2>Login</h2>
-    Username:<input type="text" name="username"><br><br>
-    Password:<input type="text" name="password"><br><br>
+    User Name: <input type="text" id="username"><br><br>
+    Password: <input type="text" id="password"><br><br>
     <input type="checkbox" name="remember">Remember me <br><br>
     <input type ="submit" value="Login">
 
+    <div id="error"></div>
+    <div id="output"></div>
+
     <p>Not registred? <a href="registration.php">Register Here</a></p>
     </form>
+    <script src="../JavaScript Files/loginvalidation.js"></script>
 </body>
-
-
 <html>
 
