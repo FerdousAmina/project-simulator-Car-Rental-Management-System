f30b41438569e7a377105d2333607a10c79b7324
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-13 19:04:51 +0600

    commit

--- Customer/MVC/View Files/emergencydirectory.php
+++ Customer/MVC/View Files/emergencydirectory.php
@@ -5,3 +5,20 @@
     <title>Emergency Directory</title>
     <link rel="stylesheet" href="../Stylesheets/emergencydirectory.css">
 </head>
+
+<body>
+    <div class="sidebar">
+        <h2>Features</h2>
+        <a href="places.php">Nearby Places to Visit</a>
+        <a href="calculator.php">Cost Calculator</a>    
+        <a href="tripnotes.php">My Notes</a>
+        <a href="feedback.php">Feedback</a>
+        <a href="emergencydirectory.php">Emergency Helplines</a>
+        <a href="logout.php">Logout</a>
+    </div>
+    <div class="main-content">
+        <h1>Emergency Helplines</h1>
+        <p>Quick access to essential service numbers for your safety during rides.</p>
+    </div>
+</body>  
+</html> 
