524676ee4095f3e00f2019ee8dc9bc2804134104
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-14 02:55:15 +0600

    commit val

---
+++ Manager/MVC/JavaScript files/addvalidation.js
@@ -0,0 +1,30 @@
+function validateForm()
+{
+    var name = document.getElementById("name") .value;
+    var phone= document.getElementById("phone") .value;
+    var nid = document.getElementById("nid") .value;
+    var age = document.getElementById("age") .value;
+
+    if (name.trim() == "")
+    {
+        alert("Name required!");
+        return false;
+    }
+    if (phone.length != 11)
+
+        {
+            alert("Phone must be 11 digit");
+            return false;
+        }
+    if (nid.lenght !=10)
+    {
+        alert("Nid must be 10 digit");
+        return false;
+    }
+    if (age <18 || age >60)
+    {
+        alert("Age must be between 18-60!");
+        return false;
+    }
+    return true;
+}
