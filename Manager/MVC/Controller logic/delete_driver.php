2724d95df99eee8c75ec34e16cc019566831011f
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-15 18:53:44 +0600

    commit

--- Manager/MVC/Controller logic/delete_driver.php
+++ Manager/MVC/Controller logic/delete_driver.php
@@ -1,15 +1,10 @@
 <?php
 include "../Model files/driverdb.php";
 
-if (isset($_GET['delete']))
-    {
-        $id = $_GET['delete'];
-        $conn->query("DELETE FROM drivers Where id=$id");
-        header("Location: delete_driver.php");
-    }
-
-    $drivers = $conn->query("SELECT * FROM drivers");
-
-    include "../view files/Drivers Information.php";
-
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $conn->query("DELETE FROM driver WHERE id=$id");
+    header("Location: ../View files/Drivers Information.php");
+    exit();
+}
 ?>
