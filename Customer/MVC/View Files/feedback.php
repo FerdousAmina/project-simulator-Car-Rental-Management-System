38e62911bd5535c908098ccb7adbbb459543ae8b
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-16 03:34:21 +0600

    commit

---
+++ Customer/MVC/View Files/feedback.php
@@ -0,0 +1,24 @@
+<?php
+include('../Controller logic/feedbackcontroller.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+    <title>Feedback</title>
+    <link rel="stylesheet" href="../Stylesheets/feedback.css">
+    <link rel="stylesheet" href="../Stylesheets/common.css">
+    <link rel="stylesheet" href="../Stylesheets/back.css">
+</head>
+<body>
+    <div class="sidebar">
+        <a href="customerdashboard.php" class="back-btn">Back</a>
+        <h2>Features</h2>
+        <a href="places.php">Nearby Places to Visit</a>
+        <a href="calculator.php">Cost Calculator</a>    
+        <a href="tripnotes.php">My Notes</a>
+        <a href="feedback.php">Feedback</a>
+        <a href="emergencydirectory.php">Emergency Helplines</a>
+        <a href="logout.php">Logout</a>
+    </div>
