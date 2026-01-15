351edfa986ec0f641ab981ac1ab0c1c23465e977
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-15 17:55:42 +0600

    commit

---
+++ Customer/MVC/View Files/tripnotes.php
@@ -0,0 +1,24 @@
+<?php
+include('../Controller logic/notescontroller.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+    <title>My Notes</title>
+    <link rel="stylesheet" href="../Stylesheets/tripnotes.css">
+</head>
+<body>
+    <div class="sidebar">
+        <h2>Features</h2>
+        <a href="places.php">Nearby Places to Visit</a>
+        <a href="calculator.php">Cost Calculator</a>    
+        <a href="tripnotes.php">My Notes</a>
+        <a href="feedback.php">Feedback</a>
+        <a href="emergencydirectory.php">Emergency Helplines</a>
+        <a href="logout.php">Logout</a>
+    </div>    
+    <div class="main-content">
+        <h1>Notes</h1>
+        <p>Keep track of your travel plans and important information with personal notes.</p>
