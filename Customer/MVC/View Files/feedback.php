7eb330b1bbe0c261c890654733a5a99122197bc2
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-16 04:00:38 +0600

    commit

--- Customer/MVC/View Files/feedback.php
+++ Customer/MVC/View Files/feedback.php
@@ -22,3 +22,53 @@ include('../Controller logic/feedbackcontroller.php');
         <a href="emergencydirectory.php">Emergency Helplines</a>
         <a href="logout.php">Logout</a>
     </div>
+    <div class="main-content">
+        <h1>Feedback</h1>
+        <p>We value your thoughts! Help us improve you ride experience by sharing your feedback with us.</p>
+
+        <div class = "form-section">
+            <form method="POST" action="feedback.php">
+                <input type="hidden" name="id" value="<?php echo $editData['id'] ?? ''; ?>">
+                <p><strong>Title:</strong></p>
+
+                <input type="text" name="title" value="<?php echo $editData['about'] ?? ''; ?>" required>
+                <p><strong>About:</strong></p>
+
+                <textarea name="feedback" required><?php echo $editData['feedback'] ?? ''; ?></textarea>
+
+                <?php if ($editData): ?>
+                    <button type="submit" name="update">Update</button>
+                    <a href="feedback.php" class="btn-cancel">Cancel</a>
+                <?php else: ?>
+                    <button type="submit" name="add">Add</button>
+                <?php endif; ?>
+            </form>
+            <p><?php echo $success; ?></p>
+            <p><?php echo $error; ?></p>
+        </div>
+
+        <?php
+        $result = mysqli_query($conn, "SELECT * FROM feedback ORDER BY id DESC");
+        while ($row = mysqli_fetch_assoc($result)){
+        ?>
+        <div class="note-container">
+        <div class="note-box">
+            <div class="note-header">
+                <h3>About: <?php echo htmlspecialchars($row['about']); ?></h3>
+            </div>
+            <div class="note-body">
+                <p>Feedback:  
+                <?php echo nl2br(htmlspecialchars($row['feedback'])); ?></p>
+            </div>
+            </div>
+            <div class="note-footer">
+                <a href="?edit=<?php echo $row['id']; ?>">Edit</a>
+                <a href="?delete=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure you want to delete feedback?');">Delete</a>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+    </div>
+</body>
+</html>
