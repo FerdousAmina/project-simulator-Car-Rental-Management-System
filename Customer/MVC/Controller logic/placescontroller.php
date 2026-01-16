b553a6d5b4ecebe0e585cf52b9d489fbd0f57414
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-16 19:38:08 +0600

    commit

--- Customer/MVC/Controller logic/placescontroller.php
+++ Customer/MVC/Controller logic/placescontroller.php
@@ -18,3 +18,29 @@ if (isset($_POST['add'])){
                 $error = mysqli_error($conn);
             }
 }
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    mysqli_query($conn, "DELETE FROM nearby_places WHERE id=$id");
+    header("Location: ../View Files/places.php");
+}
+if (isset($_POST['update'])){
+    $id = $_POST['id'];
+    $place_name = $_POST['place_name'];
+    $description = $_POST['description'];
+    $location_url = $_POST['location_url'];
+    $image_path = $_POST['image_path'];
+
+    $sql = "UPDATE nearby_places SET place_name='$place_name', description='$description', location_url='$location_url', image_path='$image_path' WHERE id=$id";
+
+    if (mysqli_query($conn, $sql)) {
+        $success = "Updated successfully!";
+    } else {
+        $error = mysqli_error($conn);
+    }
+}
+if (isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $result = mysqli_query($conn, "SELECT * FROM nearby_places WHERE id=$id");
+    $editData = mysqli_fetch_assoc($result);
+}
+?>    
