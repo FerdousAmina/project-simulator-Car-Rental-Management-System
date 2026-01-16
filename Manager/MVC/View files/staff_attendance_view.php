11a8624d9493d8ffb1ce268ae87771b10615914b
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-17 01:49:46 +0600

    commit

--- Manager/MVC/View files/staff_attendance_view.php
+++ Manager/MVC/View files/staff_attendance_view.php
@@ -8,6 +8,18 @@ $records = getAllRecords($conn);
     <title>Staff Attendance </title>
 </head>
 <body>
+<div class="container">
+<div class="input-box">
+    <h2>Record Attendance</h2>
+<div class="input-group">
+   Staff Name:<br>
+  <input type="text" id="staffName" placeholder="Enter Name"><br><br>
+   Check-in-time:<br>
+  <input type="time" id="checkIn"><br><br>
+   Check-out-time:<br>
+  <input type="time" id="checkOut"><br><br>
+  <button onclik="logAttendance()">Log Record</button>
+</div>
 
 </body>
 </html>
