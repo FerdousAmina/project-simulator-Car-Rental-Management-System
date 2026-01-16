f039f816c2e03f11e617e9e11c20959f40f02a00
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-17 01:36:10 +0600

    commit

---
+++ Manager/MVC/View files/staff_attendance_view.php
@@ -0,0 +1,13 @@
+<?php 
+include "../Controller/StaffController.php"; 
+$records = getAllRecords($conn);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Staff Attendance </title>
+</head>
+<body>
+
+</body>
+</html>
