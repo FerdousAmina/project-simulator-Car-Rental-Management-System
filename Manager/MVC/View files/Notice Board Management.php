b6893e2815ab1f1e56e69860a63c610efd4f99b8
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-16 13:58:01 +0600

    commit

--- Manager/MVC/View files/Notice Board Management.php
+++ Manager/MVC/View files/Notice Board Management.php
@@ -1,5 +1,18 @@
-<form method="POST">
-    <input type="text" name="title" placeholder="Notice Title" required>
-    <textarea name="content" Placeholder="write your notice here..."required></textarea>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Notice Board</title>
+        <link rel="stylesheet" href="../Stylesheets/notice_form.css">
+    </head>
+<body>
+<div class="main-container">
+<form method="POST" class="notice-form">
+    <h1>Post New Notice</h1><br>
+    <input type="text" name="title" placeholder="Notice Title" required><br>
+    <textarea name="content" Placeholder="write your notice here..."required></textarea><br>
     <button type ="submit" name="submit_notice">Post Notice</button>
+    </form>
+</div>
+</body>
 </form>
+<html>
