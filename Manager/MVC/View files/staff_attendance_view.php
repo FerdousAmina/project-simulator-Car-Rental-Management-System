186e85f6efad3585508ef9902e64d135bd22e150
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-17 01:52:11 +0600

    commit

--- Manager/MVC/View files/staff_attendance_view.php
+++ Manager/MVC/View files/staff_attendance_view.php
@@ -20,6 +20,19 @@ $records = getAllRecords($conn);
   <input type="time" id="checkOut"><br><br>
   <button onclik="logAttendance()">Log Record</button>
 </div>
-
+<div id="attendanceHistory">
+    <h3>Attendance Records</h3>
+    <div id="recordList">
+    <?php while($row = $records->fetch_assoc()): ?>
+    <div class="record-card">
+    <strong>Name: <?php echo htmlspecialchars($row['staff_name']); ?></strong><br>
+    <span class="bdt-time">Logged on: <?php echo date('d/m/Y, h:i A', strtotime($row['log_date'])); ?> BDT</span><br>
+    In: <?php echo date('h:i A', strtotime($row['check_in'])); ?> | 
+    Out: <?php echo $row['check_out'] ? date('h:i A', strtotime($row['check_out'])) : "Pending"; ?>
+</div>
+    <?php endwhile; ?>
+    </div>
+    </div>
+    </div>
 </body>
 </html>
