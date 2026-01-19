967496c7c101686b99dea5e94576c1875cb30722
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-19 20:19:08 +0600

    commit

--- Manager/MVC/View files/Manager dashboard.php
+++ Manager/MVC/View files/Manager dashboard.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+if (!isset($_SESSION["username"])) {
+    header("Location:../View files/login.php");
+    exit();
+}
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,12 +20,19 @@
 <div class="sidebar">
     <h1>Features</h1>
     <a href="Notice Board Management.php">Notice</a>
-    <a href="Service center Diractory.php">Service Center</a>
     <a href="Drivers Information.php">Drivers</a>
-    <a href="About us.php">About</a>
-    <a href="Staff attandence.php">Staff Attandence</a>
+    <a href="car_management.php">Import Car</a>
+    <a href="staff_attendance_view.php">Staff Attandence</a>
+    <a href="fuel_view.php">Fuel Calculator</a>
     <a href="Logout.php">Logout</a>
     
 </div>
+<script>
+    window.onpageshow = function(event) {
+        if (event.persisted) {
+            window.location.reload(); 
+        }
+    };
+</script>
 </body>
 </html>
