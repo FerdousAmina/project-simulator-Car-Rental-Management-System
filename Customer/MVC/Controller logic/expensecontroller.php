5a7f847a20e41a8cc32dbcf5e38f477a18dcb8b9
FerdousAmina <22-47822-2@student.aiub.edu>
2026-01-19 18:32:33 +0600

    commit

---
+++ Customer/MVC/Controller logic/expensecontroller.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include('../Database/expense_logger.php');
+
+$success = $error = "";
+$current_user = $_SESSION['username'];
+$editData = null;
+
+if (isset($_POST['add'])){
+    $title = $_POST['title'];
+    $cost = $_POST['cost'];
+
+    $sql = "INSERT INTO expenses (username, title, cost)
+            VALUES ('$current_user', '$title', '$cost')";
+            if (mysqli_query($conn, $sql)) {
+                $success = "Added successfully!";
+            } else {
+                $error = mysqli_error($conn);
+            }
+}
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    mysqli_query($conn, "DELETE FROM expenses WHERE id=$id AND username='$current_user'");
+    header("Location: ../View Files/expense.php");
+}
+if (isset($_POST['update'])){
+    $id = $_POST['id'];
+    $title = $_POST['title'];
+    $cost = $_POST['cost'];
+
+    $sql = "UPDATE expenses SET title='$title', cost='$cost' WHERE id=$id AND username='$current_user'";
+
+    if (mysqli_query($conn, $sql)) {
+        $success = "Updated successfully!";
+    } else {
+        $error = mysqli_error($conn);
+    }
+}
+if (isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $result = mysqli_query($conn, "SELECT * FROM expenses WHERE id=$id AND username='$current_user'");
+    $editData = mysqli_fetch_assoc($result);
+}
+?>
