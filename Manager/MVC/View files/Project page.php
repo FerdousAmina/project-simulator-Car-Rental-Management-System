ff25c6d4b2408ff48a95dfb877d95f1f46425587
Tahabi-Hossain <tahabihossain714@gmail.com>
2026-01-19 18:22:13 +0600

    commit

---
+++ Manager/MVC/View files/Project page.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>CAR RENTAL MANAGEMENT</title>
+        <link rel="stylesheet" href="../Stylesheets/loginbar.css">
+        <link rel="stylesheet" href="../Stylesheets/body.css">
+        <link rel="stylesheet" href="../Stylesheets/nav.css">
+        <link rel="stylesheet" href="../Stylesheets/home.css">
+    </head>
+<body>
+    <nav>
+        <div>
+            <a href="home.php"class="nav-home">Home</a>
+            <a href="notices_view.php"class="nav-about">Notices</a>
+            <a href="login.php" class="nav-login">Login</a>
+        </div>
+    </nav>
+
+    <div class="home">
+        <h1>Rent Your Dream Car Easily</h1><br>
+        <p>Simple . Fast . Affordable</p>
+    </div>
+
+</html>
